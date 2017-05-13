4f5ee74df1312c4b1e8a4503bf17ff1c8c3b0116
Nicolas <flaithotw@gmail.com>
2017-05-13 16:25:01 +0200

    Update disk.py

--- apple/disk.py
+++ apple/disk.py
@@ -7,6 +7,64 @@ Apple II Disk Image Reader
 - DiskDoss33: child class for Dos disk with a catalog 
 
 TODO: - PRODOS
+        See Beneath Apple ProDOS - 3.17
+        Table 3.1 ProDOS Block Conversion Table for Diskettes
+        To convert from DOS, see the bottom information:
+        DOS 3.3 SECTOR
+        For example, block 000, read track 0 sectors 0 & E
+        For example, block 017, read track 2 sectors 1 & f
+
+        The first block (block 0) is always devoted to the image of the
+        bootstrap loader. (Block 1 is the SOS bootstrap loader.) Following
+        these, and always starting at block 2, is the Volume Directory.
+        The Volume Directory is the "anchor" of the entire volume. On any
+        diskette (or hard disk for that matter) for any version of ProDOS,
+        the first or "key" block of the Volume Directory is always in the
+        same place-block 2.
+
+                    PHYSICAL SECTOR
+BLOCKS      0&2 4&6 8&A C&E 1&3 5&7 9&B D&F
+-------------------------------------------
+TRACK 0     000 001 002 003 004 005 006 007
+TRACK 1     008 009 00A 00B 00C 00D 00E 00F
+TRACK 2     010 011 012 013 014 015 016 017
+TRACK 3     018 019 01A 01B 01C 01D 01E 01F
+TRACK 4     020 021 022 023 024 025 026 027
+TRACK 5     028 029 02A 02B 02C 02D 02E 02F
+TRACK 6     030 031 032 033 034 035 036 037
+TRACK 7     038 039 03A 03B 03C 03D 03E 03F
+TRACK 8     040 041 042 043 044 045 046 047
+TRACK 9     048 049 04A 04B 04C 04D 04E 04F
+TRACK A     050 051 052 053 054 055 056 057
+TRACK B     058 059 05A 05B 05C 05D 05E 05F
+TRACK C     060 061 062 063 064 065 066 067
+TRACK D     068 069 06A 06B 06C 06D 06E 06F
+TRACK E     070 071 072 073 074 075 076 077
+TRACK F     078 079 07A 07B 07C 070 07E 07F 
+TRACK 10    080 081 082 083 084 085 086 087
+TRACK 11    088 089 08A 08D 08C 08D 08E 08F
+TRACK 12    090 091 092 093 094 095 096 097
+TRACK 13    098 099 09A 09B 09C 09D 09E 09F
+TRACK 14    0A0 0A1 0A2 0A3 0A4 0A5 0A6 0A7
+TRACK 15    0A8 0A9 0AA 0AB 0AC 0AD 0AE 0AF
+TRACK 16    0B0 0B1 0B2 0B3 0B4 0B5 0B6 0B7
+TRACK 17    0B8 0B9 0BA 0BB 0BC 0BD 0BE 0BF
+TRACK 18    0C0 0C1 0C2 0C3 0C4 0C5 0C6 0C7
+TRACK 19    0C8 0C9 0CA 0CB 0CC 0CD 0CE 0CF
+TRACK 1A    0D0 0D1 0D2 0D3 0D4 0D5 0D6 0D7
+TRACK 1B    0D8 0D9 0DA 0DB 0DC 0DD 0DE 0DF
+TRACK 1C    0E0 0E1 0E2 0E3 0E4 0E5 0E6 0E7
+TRACK 1D    0E8 0E9 0EA 0EB 0EC 0ED 0EE 0EF
+TRACK 1E    0F0 0F1 0F2 0F3 0F4 0F5 0F6 0F7
+TRACK 1F    0F8 0F9 0FA 0FB 0FC 0FD 0FE 0FF
+TRACK 20    100 101 102 103 104 105 106 107
+TRACK 21    108 109 10A 10B 10C 10D 10E 10F
+TRACK 22    110 111 112 113 114 115 116 117
+-------------------------------------------
+            0&E D&C B&A 9&8 7&6 5&4 3&2 1&F
+        0 14 13 12 11 10 9 8 7 6 5 4 3 2 1 15
+                    DOS 3.3 SECTOR
+
       - NIB format (http://boutillon.free.fr/Underground/Cours/Nibbles/Nibbles.html)
         Le .NIB permet de gérer ces disquettes au format non standardisé.
 
@@ -31,10 +89,11 @@ TODO: - PRODOS
         static int SECTOR_SIZE = 409;
 """
 from array import array
+import sys
 import os
 
 __author__ = 'Nicolas Djurovic'
-__version__ = '0.10'
+__version__ = '0.14'
 
 
 class DiskfileError(Exception):
@@ -50,9 +109,27 @@ class DiskfileError(Exception):
 
 
 class Disk:
+    # Disk File System Format
+    __FS_UNKNOW = 0
+    __FS_SEQUENTIAL = 1  # 256 bytes sector
+    __FS_NIBBLE_6656 = 2  # 6656 bytes/track
+    __FS_NIBBLE_6384 = 3  # 6384 bytes/track
+
+    # Disk System Type
+    __DSK_UNKNOW = 0
+    __DSK_DOS = 1
+    __DSK_PRODOS = 2
+    __DSK_PASCAL = 3
+
     def __init__(self, diskname):
+        # From sys, put this flag to remove Traceback display
+        sys.tracebacklimit = None
+
         # Keep the name of our disk
         self._diskname = diskname
+
+        # What kind of disk we're opening?
+        self._dsk_format = self.__DSK_UNKNOW
 
         # and init its size to 0
         self._disksize_raw = 0
@@ -61,7 +138,7 @@ class Disk:
         # the size will be given by the disk size
         self.memdisk = array('B')
 
-        # Default track/sector, so we can return the values
+        # Set a default track/sector, so we can return the values
         self.track = 0x00
         self.sector = 0x00
 
@@ -216,13 +293,13 @@ class DiskDos33(Disk):
         # Size of one sector in byte
         self._sector_size = self._vtoc['sector_size']
 
-        # Total should be 143360 bytes
+        # Total should be 143360 bytes for a usual Dos33 disk
         self._disksize = self._total_tracks * self._sector_per_track * self._sector_size
 
         # Check if the real (_disksize_raw) size of the file
         # is equal to the computed size (_disksize) returned from the VTOC
         if self._disksize_raw != self._disksize:
-            raise DiskfileError(self._diskname, 'is not a valid disk, the size is {}'.format(self._disksize_raw))
+            raise DiskfileError(self._diskname, 'is not a valid DOS disk, the size is {}'.format(self._disksize_raw))
 
     def _read_vtoc(self):
         # Reading the VTOC will give us:
@@ -259,6 +336,31 @@ class DiskDos33(Disk):
             raise DiskfileError(self._diskname, 'cannot read VTOC, not a DOS disk !!!')
 
     def catalog(self):
+        """
+            00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
+            -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+        00: 00 11 07 00 00 00 00 00 00 00 00 FF 0B 02 D0 C9  ..............PI
+        10: D8 DF D6 B2 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0  X_V2            
+        20: A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 02 00 FF 09              ....
+        30: 04 D3 C8 C5 D2 D7 CF CF C4 A0 A0 A0 A0 A0 A0 A0  .SHERWOOD       
+        40: A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 21                 !
+        50: 00 0C 09 04 CC CF C7 CF A0 A0 A0 A0 A0 A0 A0 A0  ....LOGO        
+        60: A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0                  
+                        +------- $FF, file is deleted
+                        !        get the 29th byte to get
+                        !        where was the previous
+                        v        track (here it's [1D])
+        70: A0 A0 21 00 FF 0C 04 D6 C9 C5 D7 AE D3 C5 C3 D4    !....VIEW.SECT
+        80: CF D2 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0 A0  OR              
+        90: A0 A0 A0 A0[1D]03 00 00 00 00 00 00 00 00 00 00      ............
+        A0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        B0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        C0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        D0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        E0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        F0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+        """
+
         # Get the first track and sector of our catalog
         next_track = self._first_catalog_track
         next_sector = self._first_catalog_sector
@@ -289,11 +391,13 @@ class DiskDos33(Disk):
                 # and we add the size of each filename
                 index = 0x0B + (index_file * size_of_file)
                 # Get info from File Descriptive Entry
-                fde = cat_sector[index:index + size_of_file + 1]  # .tolist()
+                fde = cat_sector[index:index + size_of_file + 1]
 
-                # Get info about the file
-                track_file = fde[0x00]
-                sector_file = fde[0x01]
+                # Get info about the file:
+                # the first T/S list of the file
+                first_track_list_file = fde[0x00]
+                first_sector_list_file = fde[0x01]
+
                 type_file = fde[0x02]
 
                 # Extract the filename
@@ -305,7 +409,7 @@ class DiskDos33(Disk):
                 file_length = (file_length_high << 8) + file_length_low
 
                 # Add the size to the total size if the file is not deleted
-                if track_file != 0xFF:
+                if first_track_list_file != 0xFF:
                     total_used_sector += file_length
                 else:
                     # If it's a deleted file, change the byte
@@ -317,7 +421,7 @@ class DiskDos33(Disk):
                 fname = ''.join(self.byte2ascii(filename)).strip()
 
                 # Add the infos extracted to our dictionnary
-                dict_program[dict_index] = [fname, file_length, track_file, sector_file, type_file]
+                dict_program[dict_index] = [fname, file_length, first_track_list_file, first_sector_list_file, type_file]
 
                 # Increment our dictionnary index
                 dict_index += 1
@@ -331,8 +435,8 @@ class DiskDos33(Disk):
             # DON'T USE program[0], it's the index
             fname = program[1][0]
             file_length = program[1][1]
-            track_file = program[1][2]
-            sector_file = program[1][3]
+            first_track_list_file = program[1][2]
+            first_sector_list_file = program[1][3]
             type_file = program[1][4]
 
             if type_file == 0:
@@ -349,13 +453,13 @@ class DiskDos33(Disk):
             # if file_length <> 0, show catalog
             if file_length != 0:
                 # If Track of the current file is equal to $FF
-                if track_file == 0xFF:  # File is deleted
+                if first_track_list_file == 0xFF:  # File is deleted
                     print('>{} {:03d} {:<30s}<'.format(tfile, file_length, fname))
                 else:
-                    print(' {} {:03d} {:30}'.format(tfile, file_length, fname))
+                    print(' {} {:03d} {:30}  [${:02X}:${:02X}]'.format(tfile, file_length, fname,
+                                                                       first_track_list_file, first_sector_list_file))
 
         total_sectors = self._total_tracks * self._sector_per_track
         total_free_sector = total_sectors - total_used_sector
-        percent = total_used_sector / total_sectors
         print('')
         print('Sectors free: {} '.format(total_free_sector))
