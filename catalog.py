b41c292c52729118e1001229b50cca5f94ea832e
Nicolas <flaithotw@gmail.com>
2017-05-14 16:00:55 +0200

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from sys import argv, exit
-from apple.disk import *
+from apple.dos import *
 
 __author__ = 'Nicolas Djurovic'
-__version__ = '0.6'
+__version__ = '0.7'
 
 
 def show_catalog(diskfile):
@@ -13,9 +13,13 @@ def show_catalog(diskfile):
         print('Cannot execute without a disk filename !!!')
         exit()
 
+    # Load our diskfile
     dsk = DiskDos33(diskfile)
+    # Display our Dos33 catalog
     dsk.catalog()
 
 if __name__ == "__main__":
+    # 'join' our arguments to avoid:
+    # TypeError: expected str, bytes or os.PathLike object, not list
     file = "".join(argv[1:])
     show_catalog(file)
